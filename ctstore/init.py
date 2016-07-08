7fdfb7addad6662e76e3302859726e4339330590
bubbleboy14 <mario.balibrera@gmail.com>
2016-07-07 22:55:12 -0700

    copy index.html instead of symlink

--- ctstore/init.py
+++ ctstore/init.py
@@ -6,13 +6,14 @@ dirs = [jsc]
 
 copies = {
 	".": ["model.py"],
-	"css": ["custom.css"]
+	"css": ["custom.css"],
+	"html": ["index.html"]
 }
 copies[jsc] = ["config.js", "data.js"]
 
 syms = {
 	"js": ["pay.js", "results.js", "store.js"],
 	"css": ["store.css"],
-	"html": ["index.html", "results.html", "checkout.html"]
+	"html": ["results.html", "checkout.html"]
 }
 syms[jsc] = ["cart.js", "search.js", "util.js"]
