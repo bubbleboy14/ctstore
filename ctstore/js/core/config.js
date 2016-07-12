d1ea6d8668b3b9e504ee7f07f391f4d2601b3ff6
bubbleboy14 <mario.balibrera@gmail.com>
2016-07-11 19:05:34 -0700

    core.config: search.buttons[]; img_fallback""

--- ctstore/js/core/config.js
+++ ctstore/js/core/config.js
@@ -3,10 +3,12 @@ core.config = {
 	data: "json", // or "db" -- for now, do everything json...
 	search: {
 		data: ["product"],
+		buttons: [],
 		blurs: [ "what are you looking for?", "what ails you?", "enter search terms" ]
 	},
 	landing: {
 		showcase: ["product"],
 		finder: ["product"]
-	}
+	},
+	img_fallback: "https://skirtingboardsdirect.com/wp-content/uploads/2014/12/Fallback-Image-400x400.png"
 };
