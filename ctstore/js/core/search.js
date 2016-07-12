bc8f2300960352e72d1130b9caf7cc03bef28061
bubbleboy14 <mario.balibrera@gmail.com>
2016-07-11 19:06:12 -0700

    improved search

--- ctstore/js/core/search.js
+++ ctstore/js/core/search.js
@@ -13,47 +13,49 @@ core.search = {
 			core.search.results(item);
 		};
 	},
-	frames: function(category, words) {
-		return core.data[category].filter(function(d) {
-			for (var i = 0; i < words.length; i++) {
-				var w = words[i];
-				for (var prop in d) {
-					if (prop == "image")
-						continue;
-					if (["label", "description"].indexOf(prop) != -1) {
-						if (d[prop].indexOf(w) != -1)
+	_filter: function(d) {
+		for (var i = 0; i < core.search._words.length; i++) {
+			var w = core.search._words[i];
+			for (var prop in d) {
+				if (["label", "description"].indexOf(prop) != -1) {
+					if (d[prop].indexOf(w) != -1)
+						return true;
+				} else if (core.config.search.buttons.indexOf(prop) != -1) {
+					for (var j = 0; j < d[prop].length; j++) {
+						var s = CT.data.get(d[prop][j]);
+						if (s.label.indexOf(w) != -1 || s.description.indexOf(w) != -1)
 							return true;
-					} else { // array props
-						var arr = d[prop];
-						for (var j = 0; j < arr.length; j++) {
-							if (arr[j].indexOf(w) != -1)
-								return true;
-						}
 					}
 				}
 			}
-		});
+		}
+	},
+	frames: function(category, words) {
+		core.search._words = words;
+		return core.data[category].filter(core.search._filter);
 	},
 	results: function(searchwords) {
 		if (location.pathname != "/results.html") {
 			location = "/results.html#" + escape(searchwords);
 			return;
 		}
-		var rnode = CT.dom.id("results"),
+		var rnode = CT.dom.id("results"), data = [],
 			words = searchwords.toLowerCase().split(" ");
 		CT.dom.setContent(rnode, "searching...");
+		core.config.search.data.forEach(function(category) {
+			var frames = core.search.frames(category, words);
+			if (frames.length) data.push({
+				label: category,
+				frames: frames
+			});
+		});
 		new CT.slider.Slider({
 			parent: rnode,
 			mode: "chunk",
 			subMode: "profile",
 			arrowPosition: "top",
 			bubblePosition: "top",
-			frames: core.config.search.data.map(function(category) {
-				return {
-					label: category,
-					frames: core.search.frames(category, words)
-				}
-			})
+			frames: data
 		});
 	}
 };
