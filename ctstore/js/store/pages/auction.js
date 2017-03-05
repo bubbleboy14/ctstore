b42c20ab46963ae60aadf8153c552b4032df91dd
bubbleboy14 <mario.balibrera@gmail.com>
2017-03-05 09:05:21 -0800

    basic auction frontend

---
+++ ctstore/js/store/pages/auction.js
@@ -0,0 +1,34 @@
+CT.require("CT.all");
+CT.require("core.config");
+CT.require("store");
+
+var loadAuction = function(auctions) {
+	if (!auctions.length)
+		return CT.dom.setContent("ctmain", "no auction today. come back soon!");
+	var auction = auctions[0], inode = CT.dom.node();
+	CT.db.one(auction.item, function(item) {
+		CT.dom.setContent(inode, [
+			CT.dom.img(item.image, "wm1-2 hm1-2 right"),
+			CT.dom.div(item.name, "bigger"),
+			CT.dom.div(item.description, "pv10")
+		]);
+	});
+	CT.dom.setContent("ctmain", CT.dom.div([
+		inode,
+		[
+			"Auction ends at: " + auction.deadline,
+			CT.parse.countdown((CT.parse.string2date(auction.deadline) - Date.now()) / 1000)
+		]
+	], "padded"));
+};
+
+CT.onload(function() {
+	store.core.util.header();
+	store.core.util.footer();
+	CT.db.get("auction", loadAuction, 1, 0, null, {
+		deadline: {
+			value: CT.parse.date2string(null, true),
+			comparator: ">="
+		}
+	});
+});
