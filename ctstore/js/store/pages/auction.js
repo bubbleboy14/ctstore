413c555c88f2bad1a8af9a772284e2e22614b13e
bubbleboy14 <mario.balibrera@gmail.com>
2017-03-08 15:31:33 -0800

    bidding frontend

--- ctstore/js/store/pages/auction.js
+++ ctstore/js/store/pages/auction.js
@@ -7,7 +7,34 @@ var loadAuction = function(auctions) {
 	if (!auctions.length)
 		return CT.dom.setContent("ctmain",
 			CT.dom.div("no auction today. come back soon!", "centered padded"));
-	var auction = auctions[0], inode = CT.dom.node();
+	var bid, izu, auction = auctions[0], inode = CT.dom.node(), bnode = CT.dom.node(),
+		u = user.core.get(), loadBid = function(bidz) {
+			CT.dom.clear(bnode);
+			if (bidz.length) {
+				bid = bidz[0];
+				izu = u.key == bid.user;
+				CT.dom.addContent(bnode, (izu ? "Your" : "High") + " bid: " + bid.amount);
+			}
+			(!bid || !izu) && CT.dom.addContent(bnode, CT.dom.button("Place Bid", function() {
+				(new CT.modal.Prompt({
+					prompt: "How much?",
+					cb: function(amount) {
+						var params = {
+							amount: amount,
+							auction: auction.key,
+							user: u.key
+						};
+						CT.net.post({
+							path: "/_bid",
+							params: params,
+							cb: function() {
+								loadBid([params]);
+							}
+						});
+					}
+				})).show();
+			}));
+		};
 	CT.db.one(auction.item, function(item) {
 		CT.dom.setContent(inode, [
 			CT.dom.img(item.image, "wm1-2 hm1-2 right"),
@@ -15,8 +42,11 @@ var loadAuction = function(auctions) {
 			CT.dom.div(item.description, "pv10")
 		]);
 	});
+	CT.db.get("bid", loadBid, 1, 0, "-amount", {
+		auction: auction.key
+	});
 	CT.dom.setContent("ctmain", CT.dom.div([
-		inode,
+		inode, bnode,
 		[
 			"Auction ends at: " + auction.deadline,
 			CT.parse.countdown((CT.parse.string2date(auction.deadline) - Date.now()) / 1000)
