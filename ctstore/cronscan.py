d06dc3428954cac3c37122dddd2da75f0d64ff91
bubbleboy14 <mario.balibrera@gmail.com>
2017-04-01 02:11:15 -0700

    cron.yaml, cronscan.py

---
+++ ctstore/cronscan.py
@@ -0,0 +1,41 @@
+from datetime import datetime, timedelta
+from cantools.web import respond, succeed, log, send_email
+from cantools import config
+from model import db, Auction, Bid
+
+CSB = """you did it!
+
+great work.
+
+here's your thing: %s
+
+enjoy!
+"""
+
+def response():
+	log("initiating cronscan", important=True)
+	az = Auction.query(
+		Auction.settled == False,
+		Auction.deadline <= datetime.datetime.now()
+	).all()
+	log("found %s active auctions"%(len(az),))
+	for a in az:
+		b = Bid.query(
+			Bid.emailed == False,
+			Bid.auction == a.key
+		).order("-amount").get()
+		if b:
+			u = b.user.get()
+			log("emailing %s"%(u.email,), 1)
+			cw = config.web
+			send_mail(
+				to = u.email,
+				subject = "you won the auction!",
+				body = CSB%("%s://%s:%s/_store?action=claim&bid=%s"%(cw.protocol,
+					cw.host, cw.port, b.key.urlsafe()),)
+			)
+			b.emailed = True
+			b.put()
+	log("cronscan succeeded -- goodbye!")
+
+respond(response, failMsg="cronscan failed")
