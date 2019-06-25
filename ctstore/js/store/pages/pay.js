f2aece902d998121ff52479af7abfae05d591726
bubbleboy14 <mario.balibrera@gmail.com>
2019-06-24 19:33:47 -0700

    pay page loader: updated cfg.payment references; dom/pay module syntax updates

--- ctstore/js/store/pages/pay.js
+++ ctstore/js/store/pages/pay.js
@@ -6,18 +6,24 @@ CT.require("store.core.cart");
 CT.require("store.core.util");
 
 CT.onload(function() {
+	var pnode = CT.dom.div(null, "w1 h1"),
+		pcfg = core.config.ctstore.payment;
 	CT.initCore();
-	var pnode = CT.dom.node("", "div", "w1 h1");
 	store.core.cart.load();
-	CT.dom.setContent(CT.dom.id("items"), store.core.cart.list());
-	CT.dom.setContent(CT.dom.id("payment"), pnode);
-	new CT.pay.Form({
-		parent: pnode
+	CT.dom.setContent("items", store.core.cart.list());
+	CT.dom.setContent("payment", pnode);
+	CT.pay.init({ // TODO: fuller integration, including cc!
+		mode: pcfg.mode,
+		cb: function() {
+			new CT.pay.Form({
+				parent: pnode
+			});
+		}
 	});
-	if (core.config.ctstore.pay_notice) {
+	if (pcfg.notice) {
 		(new CT.modal.Modal({
 			transition: "slide",
-			content: core.config.ctstore.pay_notice
+			content: pcfg.notice
 		})).show();
 	}
 });
