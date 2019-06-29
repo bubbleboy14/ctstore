76ec20aa1d8d36c77a8b4f0e4ce9a690f202d7ec
bubbleboy14 <mario.balibrera@gmail.com>
2019-06-28 18:37:52 -0700

    results page loader: core import (not just core.config)

--- ctstore/js/store/pages/results.js
+++ ctstore/js/store/pages/results.js
@@ -1,5 +1,5 @@
 CT.require("CT.all");
-CT.require("core.config");
+CT.require("core");
 CT.require("store");
 CT.require("user.core");
 CT.dom.addStyle(null, "/css/layouts/results/" + core.config.ctstore.search.layout + ".css");
